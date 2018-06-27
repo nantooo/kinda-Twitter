4ee48420890adca40871305f66312da5e48e8f4d
大川　楠人 <nanto.070503@gmail.com>
2018-06-27 21:12:45 +0900

    last commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -43,7 +51,10 @@ class User < ActiveRecord::Base
     update_attribute(:activated_at, Time.zone.now)
   end
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # 有効化用のメールを送信する
@@ -64,7 +75,20 @@ class User < ActiveRecord::Base
     def password_reset_expired?
       reset_sent_at < 2.hours.ago
     end
+ # ユーザーをフォローする
+ def follow(other_user)
+  active_relationships.create(followed_id: other_user.id)
+end
 
+# ユーザーをアンフォローする
+def unfollow(other_user)
+  active_relationships.find_by(followed_id: other_user.id).destroy
+end
+
+# 現在のユーザーがフォローしてたらtrueを返す
+def following?(other_user)
+  following.include?(other_user)
+end
     private
 
     # メールアドレスをすべて小文字にする
