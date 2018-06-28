258b38e089c08ea2914fe84ea962dd0c81363c1d
大川　楠人 <nanto.070503@gmail.com>
2018-06-28 17:15:46 +0900

    oppai

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
 module ApplicationHelper
     def full_title(page_title)
-        base_title = "アプリ名" # 自分のアプリ名を設定する
+        base_title = "The Movie World" # 自分のアプリ名を設定する
         if page_title.empty?
           base_title
         else
           "#{page_title} | #{base_title}"
         end
       end
+
+
+   def date_format(datetime)
+    time_ago_in_words(datetime) + '前'
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name:  "Nanto Okawa",
-    email: "nanto.070503@gmail.com",
-    password:              "nanto1097",
-    password_confirmation: "nanto1097",
+    email: "nanto@gmail.com",
+    password:              "123456789",
+    password_confirmation: "123456789",
     admin: true,
     activated: true,
     activated_at: Time.zone.now)
